f12275931465b98569f27309685a2a6f3574bc9a
Gaspar Brito <69097531+Gfranbm@users.noreply.github.com>
2021-12-31 11:44:02 -0600

    Update and rename bundle.js to App.js
    cambio de bundle.min.js a App.js

---
+++ build/js/App.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function(){
+    scrollNav();
+
+    navegacionFija();
+
+});
+
+
+function navegacionFija(){
+    const barra = document.querySelector('a');
+
+    //Registrar el Intersection Observer
+    const observer = new IntersectionObserver(function(entries){
+        if(entries[0].isIntersecting){
+            barra.classList.remove('active')
+        }else{
+            barra.classList.add('active');    
+        }
+    });
+
+    //Elento a Observar
+    observer.observe(document.querySelector(''))
+
+}
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach(function(enlace){
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    });
+}
